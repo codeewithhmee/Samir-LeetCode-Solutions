961a67c7ef50a465fc283d085f464e935f8b0aa4
codeewithhmee <samirbhandari1231@gmail.com>
2025-12-13 10:05:54 +0545

    Time: 2 ms (92.83%), Space: 45.8 MB (19.59%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,28 +2,25 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans=new ArrayList<>();
         List<Integer> temp=new ArrayList<>();
-        helper(candidates,target,0,ans,temp);
+        helper(candidates,ans,temp,0,target);
         return ans;
+
     }
-    public void helper(int[] arr,int target,int idx,List<List<Integer>> ans,List<Integer> temp){
+    public void helper(int[] arr,List<List<Integer>> ans,List<Integer> temp,int idx,int target){
         //base case
-        if(target<=0 || idx==arr.length){
+        if(idx>=arr.length || target<=0){
             if(target==0){
-                List<Integer> a=new ArrayList<>(temp);
-                ans.add(a);
+                ans.add(new ArrayList<>(temp));
             }
             return;
         }
-
-        //include element
+        //dont move forward or put the same element in ds
         temp.add(arr[idx]);
-        helper(arr,target-arr[idx],idx,ans,temp);
-
-
-        //exclude element
+        helper(arr,ans,temp,idx,target-arr[idx]);
+        //move forward
         temp.remove(temp.size()-1);
-        helper(arr,target,idx+1,ans,temp);
-
-
+        helper(arr,ans,temp,idx+1,target);
     }
+   
+      
 }
