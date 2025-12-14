eafcb876ba823b347dfb949194ac18f3b705fc57
codeewithhmee <samirbhandari1231@gmail.com>
2025-12-14 20:01:58 +0545

    Time: 0 ms (100%), Space: 45.4 MB (39.59%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,31 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
        List<List<Integer>> ans=new ArrayList<>();
-       List<Integer> temp=new ArrayList<>(); 
-       int[] check=new int[nums.length];
-       helper(ans,temp,check,nums);
+       helper(ans,0,nums);
        return ans;
     }
-    public void helper(List<List<Integer>> ans,  List<Integer> temp,int[] check,int[] arr){
-        if(temp.size()>=arr.length){
-            ans.add(new ArrayList<>(temp));
+ 
+    public void helper(List<List<Integer>> ans,int idx,int[] arr){
+        //base case
+        if(idx==arr.length){
+           List<Integer> perm = new ArrayList<>();
+            for (int x : arr) perm.add(x);
+            ans.add(perm);
             return;
         }
-        for(int i=0;i<arr.length;i++){
-            if(check[i]==1)continue;
-            temp.add(arr[i]);
-            check[i]=1;
-            helper(ans,temp,check,arr);
-            temp.remove(temp.size()-1);
-            check[i]=0;
-        }
+        for(int i=idx;i<arr.length;i++){
+            //swapping arr[idx] with arr[i]
+            int temp=arr[idx];
+            arr[idx]=arr[i];
+            arr[i]=temp;
 
+            helper(ans,idx+1,arr);
+            //back track
+            temp=arr[idx];
+            arr[idx]=arr[i];
+            arr[i]=temp;
+
+
+        }
     }
 }
