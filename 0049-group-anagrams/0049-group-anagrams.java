65dbbf2fc0398d39aef1f4dc36fa7e857a1fbbdc
codeewithhmee <samirbhandari1231@gmail.com>
2025-12-15 20:54:43 +0545

    Time: 6 ms (98.96%), Space: 49.8 MB (32.8%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,35 +1,23 @@
 class Solution {
-    public boolean isAnargam(String s1, String s2) {
-        if (s1.length() != s2.length())
-            return false;
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
-        for (int i = 0; i < s1.length(); i++) {
-            freq1[s1.charAt(i) - 'a']++;
-            freq2[s2.charAt(i) - 'a']++;
-        }
-        return Arrays.equals(freq1, freq2);
-
-    }
-
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> ans = new ArrayList<>();
-
-        int[] check = new int[strs.length];
-
-        for (int i = 0; i < strs.length; i++) {
-            if (check[i] == 1)
-                continue;
-            List<String> temp = new ArrayList<>();
-            temp.add(strs[i]);
-            for (int j = i + 1; j < strs.length; j++) {
-                if (isAnargam(strs[i], strs[j])) {
-                    temp.add(strs[j]);
-                    check[j] = 1;
-                }
-            }
-            ans.add(temp);
+        List<List<String>> ans=new ArrayList<>();
+     HashMap<String,List<String>> map=new HashMap<>();
+     for(String s:strs){
+     char[] c = s.toCharArray(); // convert string to char array
+Arrays.sort(c); 
+        String temp=new String(c);
+        if(map.containsKey(temp)){
+            map.get(temp).add(s);
+        }else{
+            List<String> list=new ArrayList<>();
+            list.add(s);
+            map.put(temp,list);
         }
-        return ans;
+     }
+     for(String key:map.keySet()){
+        ans.add(map.get(key));
+     }
+     return ans;
+
     }
 }
