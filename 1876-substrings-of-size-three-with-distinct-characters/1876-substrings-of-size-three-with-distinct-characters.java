20362005b70390cd6a7e5d9c6a99c509d6082353
codeewithhmee <samirbhandari1231@gmail.com>
2025-12-22 22:15:28 +0545

    Time: 1 ms (93.25%), Space: 42.9 MB (49.46%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -1,18 +1,27 @@
 class Solution {
     public int countGoodSubstrings(String s) {
-        int[] freq=new int[26];
-        int l=0;
-        int ans=0;
-        for(int r=0;r<s.length();r++){
-            freq[s.charAt(r)-'a']++;
-            if(r-l+1<3)continue;
-            int count=0;
-            for(int n:freq){
-                if(n==1)count++;
-            }
-            if(count==3)ans++;
-            freq[s.charAt(l)-'a']--;
-            l++;
+        // int[] freq=new int[26];
+        // int l=0;
+        // int ans=0;
+        // for(int r=0;r<s.length();r++){
+        //     freq[s.charAt(r)-'a']++;
+        //     if(r-l+1<3)continue;
+        //     int count=0;
+        //     for(int n:freq){
+        //         if(n==1)count++;
+        //     }
+        //     if(count==3)ans++;
+        //     freq[s.charAt(l)-'a']--;
+        //     l++;
+        // }
+        // return ans;
+
+int ans=0;
+        for(int i=0;i<s.length()-2;i++){
+            char a=s.charAt(i);
+            char b=s.charAt(i+1);
+            char c=s.charAt(i+2);
+            if(a!=b && a!=c && b!=c)ans++;
         }
         return ans;
     }
