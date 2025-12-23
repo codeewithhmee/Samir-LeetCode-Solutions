d48edbc9b4d1f1170786fcb2751bf53836babe00
codeewithhmee <samirbhandari1231@gmail.com>
2025-12-23 22:19:29 +0545

    Time: 6 ms (4.06%), Space: 46.2 MB (6.47%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,9 +1,7 @@
 class Solution {
     public String decodeString(String s) {
-       
         String finalans="";
         Stack<Character> st=new Stack<>();
-     
         for(char c:s.toCharArray()){
             if(c==']'){
                 String ans = "";
@@ -12,19 +10,21 @@ class Solution {
                     ans=ch+ans;
                 }
                 if(st.size()>0)st.pop();//removing ']'
+
                 //now we calculate the how many times
                 String f="";
                 while(st.size()>0 && st.peek()>='0' && st.peek()<='9'){
                     f=st.pop()+f;
                 }
                 int freq=Integer.parseInt(f);
-                System.out.println(freq);
-                System.out.println(ans);
+
+              
                 //now we make a ans how many times
                 String str=ans;
                 for(int i=0;i<freq-1;i++){
                     ans=ans+str;
                 }
+                
                 for (char ch : ans.toCharArray()) st.push(ch);
                 
             }else{
