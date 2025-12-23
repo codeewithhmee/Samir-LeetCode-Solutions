e947cc2372237c2c2b2f9c3aac42f76cf17f9276
codeewithhmee <samirbhandari1231@gmail.com>
2025-12-23 20:51:58 +0545

    Time: 134 ms (68.43%), Space: 43 MB (52.58%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,38 +1,36 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-    int[][] visited=new int[board.length][board[0].length];
-    for(int i=0;i<board.length;i++){
-        for(int j=0;j<board[0].length;j++){
-            if(helper(board,new StringBuilder(),word,i,j,visited))return true;
+        int rows = board.length;
+        int cols = board[0].length;
+        boolean[][] visited = new boolean[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (helper(board, word, i, j, 0, visited)) return true;
+            }
         }
+        return false;
     }
-    return false ;
 
-    }
-    public boolean helper(char[][] board,StringBuilder sb,String word,int cr,int cc,int[][] visited){
-        //base cases
-        if (sb.length()>0 && sb.charAt(sb.length()-1) != word.charAt(sb.length()-1))return false;
-        if(cr==board.length || cc==board[0].length || cr<0 || cc<0  || sb.length()>=word.length() || visited[cr][cc]==1){
-            String str=sb.toString();
-            if(str.equals(word))return true;
-            return false;
-        }
-        visited[cr][cc]=1;
-        sb.append(board[cr][cc]);
-        //move right
-        boolean r=helper(board,sb,word,cr,cc+1,visited);
-        //move down
-        boolean d=helper(board,sb,word,cr-1,cc,visited);
-        //move left
-        boolean l=helper(board,sb,word,cr,cc-1,visited);
-        //move up
-       boolean u= helper(board,sb,word,cr+1,cc,visited);
-        //backtrack
-        visited[cr][cc]=0;
-        sb.deleteCharAt(sb.length() - 1);
+    private boolean helper(char[][] board, String word, int cr, int cc, int idx, boolean[][] visited) {
+        // base cases
+        if (cr < 0 || cc < 0 || cr >= board.length || cc >= board[0].length) return false;
+        if (visited[cr][cc]) return false;
+        if (board[cr][cc] != word.charAt(idx)) return false;
+        if (idx == word.length() - 1) return true;
 
-        return r||d||l||u;
+        // visited
+        visited[cr][cc] = true;
 
-         
+       
+        boolean found = helper(board, word, cr, cc + 1, idx + 1, visited) || // right
+                        helper(board, word, cr + 1, cc, idx + 1, visited) || // down
+                        helper(board, word, cr, cc - 1, idx + 1, visited) || // left
+                        helper(board, word, cr - 1, cc, idx + 1, visited);   // up
+
+        // backtrack
+        visited[cr][cc] = false;
+
+        return found;
     }
 }
