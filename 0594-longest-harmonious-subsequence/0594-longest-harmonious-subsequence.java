fd7fec7ccd265aa05d26e42bd63659c1a7eadd77
codeewithhmee <samirbhandari1231@gmail.com>
2025-12-24 21:38:10 +0545

    Time: 18 ms (24.82%), Space: 47.8 MB (80.07%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -6,10 +6,12 @@ class Solution {
             map.put(n,map.getOrDefault(n,0)+1);
         }
         int ans=0;
+        int f=0;
         for(int n : map.keySet()){
-            if(map.containsKey(n+1)){
-                ans = Math.max(ans, map.get(n) + map.get(n+1));
-            }
+           if(map.containsKey(n+1)){
+            f=map.get(n+1)+map.get(n);
+           }
+           ans=Math.max(ans,f);
         }
         return ans;
 
