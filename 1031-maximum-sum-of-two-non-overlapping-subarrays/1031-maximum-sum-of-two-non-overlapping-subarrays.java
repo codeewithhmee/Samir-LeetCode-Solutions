c23c5780f9330d790ca54aa50d29ef12f4eaa566
codeewithhmee <samirbhandari1231@gmail.com>
2025-12-24 18:47:18 +0545

    Time: 4 ms (28.89%), Space: 44 MB (61.85%) - LeetHub

---
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int maxSumTwoNoOverlap(int[] nums, int firstLen, int secondLen) {
+        //for first length
+        int l=0;
+        int sum=0;
+        int ans=0;
+        for(int r=0;r<nums.length;r++){
+            sum+=nums[r];
+            if(r-l+1<firstLen)continue;
+            int max=findMax(nums,l,r,secondLen);
+            ans=Math.max(ans,sum+max);
+            //
+            sum-=nums[l];
+            l++;
+
+        }
+        return ans;
+    }
+    public int findMax(int[] nums,int s,int e,int secondLen){
+        //from 0 to l-1
+        int max=0;
+        int sum=0;
+        int l=0;
+        for(int r=0;r<s;r++){
+            sum+=nums[r];
+            if(r-l+1<secondLen)continue;
+            if(r-l+1==secondLen){
+ max=Math.max(sum,max);
+            }
+           
+            sum-=nums[l];
+            l++;
+        }
+        //form r+1 to n
+        int max1=0;
+        int sum1=0;
+        int l1=e+1;
+        for(int r=e+1;r<nums.length;r++){
+             sum1+=nums[r];
+            if(r-l1+1<secondLen)continue;
+            if(r-l1+1==secondLen){
+ max1=Math.max(sum1,max1);
+            }
+           
+            sum1-=nums[l1];
+            l1++;
+        }
+        return Math.max(max,max1);
+    }
+}
