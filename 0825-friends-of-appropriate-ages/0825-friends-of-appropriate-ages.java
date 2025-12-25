4cf8be535bc5a52b5b04b4a7e453e680e858f010
Code <samirbhandari1231@gmail.com>
2025-12-25 21:56:28 +0545

    Time: 1114 ms (6.68%), Space: 48.9 MB (10.62%) - LeetHub

--- 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.java
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < ages.length; i++) {
             int threshold = ages[i] / 2 + 7;
             for (int j = i - 1; j >= 0; j--) {
-                if (ages[j] > threshold && ages[j] <= ages[i]) { 
+                if (ages[j] > threshold) { 
                     if(ages[i]==ages[j]){
                         ans+=2;
                     }else{
