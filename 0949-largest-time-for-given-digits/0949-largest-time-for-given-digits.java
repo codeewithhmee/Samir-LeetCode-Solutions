2314f5e2dd1d3115d0add020bb7203201de37c4a
Code <samirbhandari1231@gmail.com>
2025-12-27 15:55:35 +0545

    Time: 6 ms (59.01%), Space: 43.2 MB (80%) - LeetHub

---
+++ 0949-largest-time-for-given-digits/0949-largest-time-for-given-digits.java
@@ -0,0 +1,48 @@
+class Solution {
+    int hour=-1,min=-1;
+    public String largestTimeFromDigits(int[] arr) {
+        permu(arr,0);
+        if(hour==-1)return "";
+        System.out.println(hour);
+        System.out.println(min);
+         String h=String.valueOf(hour);
+         String m=String.valueOf(min);
+
+         if(hour<10){
+            h="0"+h;
+         }
+         if(min<10){
+            m="0"+m;
+         }
+
+        return h+":"+m;
+        
+
+    }
+    public void permu(int[] arr,int idx){
+        if(idx==arr.length){
+            int h=arr[0]*10+arr[1];
+            int m=arr[2]*10+arr[3];
+            if(h<24 && m<59){
+                if(h>hour){
+                    hour=h;
+                    min=m;
+                }else if(h==hour && m>min){
+                    min=m;
+                }
+            }
+            return;
+        }
+        for(int i=idx;i<arr.length;i++){
+            //swap
+            int temp=arr[idx];
+            arr[idx]=arr[i];
+            arr[i]=temp;
+            permu(arr,idx+1);
+            //backtrack
+             temp=arr[idx];
+            arr[idx]=arr[i];
+            arr[i]=temp;
+        }
+    }
+}
