686b8148a695855099b1283f036b06c07069aa93
Code <samirbhandari1231@gmail.com>
2025-12-27 20:16:53 +0545

    Time: 3 ms (41.59%), Space: 48.1 MB (7.25%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,10 +1,20 @@
 class Solution {
     public boolean canJump(int[] nums) {
-       int max=0;
-       for(int i=0;i<nums.length;i++){
-        if(i>max)return false;
-        max=Math.max(i+nums[i],max);
-       }
-return true;
+        // int max = 0;
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (i > max)
+        //         return false;
+        //     max = Math.max(i + nums[i], max);
+        // }
+        // return true;
+        //next method
+        int destination=nums.length-1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if(i+nums[i]>=destination){
+                destination=i;
+            }
+        }
+        System.out.println(destination);
+        return destination==0;
     }
 }
